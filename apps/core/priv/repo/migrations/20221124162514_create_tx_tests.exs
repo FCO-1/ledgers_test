8979fc77f3a8395112fdc67585c46dd43926c60f
FCO-1 <pacomendez012@gmail.com>
2022-11-24 11:00:40 -0600

    cambio de tabla

--- apps/core/priv/repo/migrations/20221124162514_create_tx_tests.exs
+++ apps/core/priv/repo/migrations/20221124162514_create_tx_tests.exs
@@ -2,11 +2,13 @@ defmodule Core.Repo.Migrations.CreateTxTests do
   use Ecto.Migration
 
   def change do
-    create table(:tx_tests, primary_key: false) do
+    schema = "transactions"
+    table = "tx_tests"
+    create table(:tx_tests, primary_key: false, prefix: schema) do
       add :id, :binary_id, primary_key: true
       add :tx_table, :string
       add :reference, :string
-      add :ammount, :string
+      add :ammount, :numeric
       add :way, :string
       add :owner, :string
 
