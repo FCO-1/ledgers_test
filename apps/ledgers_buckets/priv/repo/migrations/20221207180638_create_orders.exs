f10a15722bd90ce9c360cc4070a3b34f62c8c35b
FCO-1 <pacomendez012@gmail.com>
2022-12-07 12:08:57 -0600

    migracion

--- apps/ledgers_buckets/priv/repo/migrations/20221207180638_create_orders.exs
+++ apps/ledgers_buckets/priv/repo/migrations/20221207180638_create_orders.exs
@@ -2,15 +2,17 @@ defmodule LedgersBuckets.Repo.Migrations.CreateOrders do
   use Ecto.Migration
 
   def change do
-    create table(:orders, primary_key: false) do
+    schema = "buckets"
+    table = "orders"
+    create table(:orders, primary_key: false, prefix: schema) do
       add :id, :binary_id, primary_key: true
-      add :origin, :string
-      add :amount, :string
-      add :type, :string
-      add :status, :string
-      add :state, :string
-      add :owner, :string
-      add :destination, :string
+      add :origin, :citext
+      add :amount, :numeric
+      add :type, :citext
+      add :status, :citext
+      add :state, :citext
+      add :owner, :citext
+      add :destination, :citext
       add :extras, :map
 
       timestamps()
