814bc34c27f424d32b2b700efcfe959c5b0720d7
FCO-1 <pacomendez012@gmail.com>
2022-12-08 13:12:48 -0600

    metodo

--- apps/ledgers_buckets/lib/ledgers_buckets/context/buckets.ex
+++ apps/ledgers_buckets/lib/ledgers_buckets/context/buckets.ex
@@ -74,14 +74,13 @@ defmodule LedgersBuckets.Buckets do
 
 
   def create_new_bucket_transaccion_for_swap(attrs, list_ids) do
-    sum = get_sum_buckets_by_list_ids(list_ids)
-    new_attrs = Map.merge(attrs, %{"amount" => sum})
+
     Repo.transaction(fn ->
-      with {:ok, bucket_txs} <- build_bucket_txs(new_attrs) |> create_bucket_txs(),
-      {:ok, _bucket_tx_from} <- build_tx_from(new_attrs) |> create_bucket_tx_from(),
-      {:ok, _bucket_tx_to} <- build_tx_to(new_attrs, bucket_txs) |> create_bucket_tx_to(),
+      with {:ok, bucket_txs} <- build_bucket_txs(attrs) |> create_bucket_txs(),
+      {:ok, _bucket_tx_from} <- build_tx_from(attrs) |> create_bucket_tx_from(),
+      {:ok, _bucket_tx_to} <- build_tx_to(attrs, bucket_txs) |> create_bucket_tx_to(),
       {:ok, _bucketsdeleted} <- delete_many_buckets(list_ids),
-      {:ok, _created_new_bucket} <- build_bucket(new_attrs, bucket_txs) |> create_bucket() do
+      {:ok, _created_new_bucket} <- build_bucket(attrs, bucket_txs) |> create_bucket() do
         bucket_txs
       else
         {:error, changeset} ->
--- apps/ledgers_buckets/lib/ledgers_buckets/domain/buckets_domain.ex
+++ apps/ledgers_buckets/lib/ledgers_buckets/domain/buckets_domain.ex
@@ -2,6 +2,15 @@ defmodule LedgersBuckets.Domain.BucketsDomain do
   alias LedgersBuckets.Buckets
 
 
+  def create_new_bucket_transaccion(attrs) do
+    Buckets.create_new_bucket_transaccion(attrs)
+  end
+
+  def create_new_buckets_for_swap(attrs, list_buckets_ids) do
+    sum = Buckets.get_sum_buckets_by_list_ids(list_buckets_ids)
+    Buckets.create_new_bucket_transaccion_for_swap(Map.merge(attrs, %{"amount" => sum}), list_buckets_ids)
+  end
+
   def create_new_buckets_for_partitions(attrs, amount, list_buckets_ids) do
     sum_from_buckets = Buckets.get_sum_buckets_by_list_ids(list_buckets_ids)
     if amount <= sum_from_buckets do
