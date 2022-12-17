e2a26965865777bbc5c48fb3dfb013c9bd636bf8
FCO-1 <pacomendez012@gmail.com>
2022-12-16 19:55:40 -0600

    generacion de dominio para orders

--- apps/ledgers_buckets/lib/ledgers_buckets/domain/orders_domain.ex
+++ apps/ledgers_buckets/lib/ledgers_buckets/domain/orders_domain.ex
@@ -1,13 +1,8 @@
-defmodule LedgersBuckets.Context.Orders do
+defmodule LedgersBuckets.Domaim.Orders do
   @moduledoc """
   The Orders context.
   """
-
-  import Ecto.Query, warn: false
-  alias LedgersBuckets.Repo
-
-  alias LedgersBuckets.Orders.Order
-  alias LedgersBuckets.Orders.OrderBucketTxs
+  alias LedgersBuckets.Context.Orders
 
 
   @doc """
@@ -20,7 +15,7 @@ defmodule LedgersBuckets.Context.Orders do
 
   """
   def list_orders do
-    Repo.all(Order)
+    Orders.list_orders()
   end
 
   @doc """
@@ -37,7 +32,7 @@ defmodule LedgersBuckets.Context.Orders do
       ** (Ecto.NoResultsError)
 
   """
-  def get_order!(id), do: Repo.get!(Order, id)
+  def get_order!(id), do: Orders.get_order!(id)
 
   @doc """
   Creates a order.
@@ -51,10 +46,8 @@ defmodule LedgersBuckets.Context.Orders do
       {:error, %Ecto.Changeset{}}
 
   """
-  def create_order(attrs \\ %{}) do
-    %Order{}
-    |> Order.changeset(attrs)
-    |> Repo.insert()
+  def create_order(attrs) do
+    Orders.create_order(attrs)
   end
 
   @doc """
@@ -69,10 +62,8 @@ defmodule LedgersBuckets.Context.Orders do
       {:error, %Ecto.Changeset{}}
 
   """
-  def update_order(%Order{} = order, attrs) do
-    order
-    |> Order.changeset(attrs)
-    |> Repo.update()
+  def update_order(order, attrs) do
+    Orders.update_order(order, attrs)
   end
 
   @doc """
@@ -87,8 +78,8 @@ defmodule LedgersBuckets.Context.Orders do
       {:error, %Ecto.Changeset{}}
 
   """
-  def delete_order(%Order{} = order) do
-    Repo.delete(order)
+  def delete_order(order) do
+    Orders.delete_order(order)
   end
 
   @doc """
@@ -100,12 +91,16 @@ defmodule LedgersBuckets.Context.Orders do
       %Ecto.Changeset{data: %Order{}}
 
   """
-  def change_order(%Order{} = order, attrs \\ %{}) do
-    Order.changeset(order, attrs)
+  def change_order(order) do
+    Orders.change_order(order)
+  end
+  def change_order(order, attrs) do
+    Orders.change_order(order, attrs)
   end
 
   ##########################################################
   #LedgersBuckets.Orders.OrderBucketTxs
+  ##########################################################
 
   @doc """
   Returns the list of order_bucket_txs.
@@ -117,7 +112,7 @@ defmodule LedgersBuckets.Context.Orders do
 
   """
   def list_order_bucket_txs do
-    Repo.all(OrderBucketTxs)
+    Orders.list_order_bucket_txs()
   end
 
   @doc """
@@ -134,7 +129,7 @@ defmodule LedgersBuckets.Context.Orders do
       ** (Ecto.NoResultsError)
 
   """
-  def get_order_bucket_txs!(id), do: Repo.get!(OrderBucketTxs, id)
+  def get_order_bucket_txs!(id), do: Orders.get_order_bucket_txs!(id)
 
   @doc """
   Creates a order_bucket_txs.
@@ -148,10 +143,8 @@ defmodule LedgersBuckets.Context.Orders do
       {:error, %Ecto.Changeset{}}
 
   """
-  def create_order_bucket_txs(attrs \\ %{}) do
-    %OrderBucketTxs{}
-    |> OrderBucketTxs.changeset(attrs)
-    |> Repo.insert()
+  def create_order_bucket_txs(attrr) do
+    Orders.create_order_bucket_txs(attrr)
   end
 
   @doc """
@@ -166,10 +159,8 @@ defmodule LedgersBuckets.Context.Orders do
       {:error, %Ecto.Changeset{}}
 
   """
-  def update_order_bucket_txs(%OrderBucketTxs{} = order_bucket_txs, attrs) do
-    order_bucket_txs
-    |> OrderBucketTxs.changeset(attrs)
-    |> Repo.update()
+  def update_order_bucket_txs(order_bucket_txs, attrs) do
+    Orders.update_order_bucket_txs(order_bucket_txs, attrs)
   end
 
   @doc """
@@ -184,8 +175,8 @@ defmodule LedgersBuckets.Context.Orders do
       {:error, %Ecto.Changeset{}}
 
   """
-  def delete_order_bucket_txs(%OrderBucketTxs{} = order_bucket_txs) do
-    Repo.delete(order_bucket_txs)
+  def delete_order_bucket_txs(order_bucket_txs) do
+    Orders.delete_order_bucket_txs(order_bucket_txs)
   end
 
   @doc """
@@ -197,8 +188,11 @@ defmodule LedgersBuckets.Context.Orders do
       %Ecto.Changeset{data: %OrderBucketTxs{}}
 
   """
-  def change_order_bucket_txs(%OrderBucketTxs{} = order_bucket_txs, attrs \\ %{}) do
-    OrderBucketTxs.changeset(order_bucket_txs, attrs)
+  def change_order_bucket_txs(order_bucket_txs) do
+    Orders.change_order_bucket_txs(order_bucket_txs)
+  end
+  def change_order_bucket_txs(order_bucket_txs, attrs) do
+    Orders.change_order_bucket_txs(order_bucket_txs, attrs)
   end
 
 end
